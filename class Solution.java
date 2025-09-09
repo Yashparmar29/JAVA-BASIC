dad64f13d9781aee5ee68cbab36588be2f9a33f1
Yashparmar29 <parmaryas29@gmail.com>
2025-09-09 22:25:57 +0530

    Add files via upload

---
+++ class Solution.java
@@ -0,0 +1,13 @@
+class Solution
+{
+    public int[] twoSum(int[] nums, int target) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter no 1:- ");
+        int no1 = sc.nextInt();
+        System.out.print("enter no 2:- ");
+        int no2 = sc.nextInt();
+
+        System.out.print("sum :- " + (no1 + no2));    
+    }
+}
