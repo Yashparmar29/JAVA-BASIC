495667377e659f8331eb1bbef4845c0283684cf2
Parmar Yash <parmaryas29@gmail.com>
2025-09-15 23:04:09 +0530

    Add files via upload

---
+++ get_method.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class get_method 
+{
+    public static void main(String[] args)
+    {
+        int no = 5;
+        Scanner sc = new Scannet(System.in);
+        System.out.print("enter no :- ");
+        int pos = sc.nextInt();
+    }    
+}
